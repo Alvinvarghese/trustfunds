be6ca62e7a904bed1771c3c70243503e95a7fbc5
Alvin Varghese <alvin19official@gmail.com>
2024-04-09 12:34:48 +0530

    feat: search functionality

--- src/app/campaigns/search/[slug]/page.js
+++ src/app/campaigns/search/[slug]/page.js
@@ -1,47 +1,45 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import Loading from '@/components/Campaigns/components/Loading';
 import Error from '@/components/Campaigns/components/Error';
-// import CampaignCard from '@/components/CampaignCard/Card';
 import { searchCampaignsAPI } from '@/axios';
-// import CardLayout from '@/components/CampaignCard/CardLayout';
 import AllCampaigns from '@/components/Campaigns/components/AllCampaigns';
+import { toastError } from '@/lib/toast';
 
 const SearchResults = (props) => {
   const query = props.params.slug
   const [status, setStatus] = useState('loading');
   const [campaigns, setCampaigns] = useState([]);
 
-  useEffect(() => {
-    const fetchSearchResults = async () => {
-      try {
-        const res = await searchCampaignsAPI(query);
-        if (res.status === 200) {
-          setCampaigns(res.data.campaigns);
-          setStatus('success');
-        }
-      } catch (error) {
-        setStatus('error');
-        console.error(error);
+  const fetchSearchResults = useCallback(async () => {
+    try {
+      setStatus("loading")
+      const res = await searchCampaignsAPI(query);
+      if (res.status === 200) {
+        setCampaigns(res.data.campaigns);
+        setStatus('success');
       }
-    };
+    } catch (error) {
+      setStatus('error');
+      toastError(error?.response?.data?.error || "Something went wrong whle fetching campaigns. Try again later!")
+      console.log(error);
+    }
+  }, [query]);
 
+  useEffect(() => {
     fetchSearchResults();
   }, [query]);
 
   return (
     <div className='h-screen'>
       <div className="p-4">
-        <h1 className="text-3xl font-bold mb-4">Search Results for <span className='italic'>"{query}"</span></h1>
+        <h1 className="text-3xl font-bold mb-4 leading-snug">Search Results for <span className='italic'>"{query}"</span></h1>
         {status === 'loading' && <Loading />}
-        {status === 'error' && <Error />}
+        {status === 'error' && <Error fetchData={fetchSearchResults} />}
         {status === 'success' && campaigns.length > 0 && (
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-            {campaigns.map(campaign => (
-              // <Card key={campaign.id} campaign={campaign} />
-              <AllCampaigns data={campaigns} />
-            ))}
+            <AllCampaigns data={campaigns} />
           </div>
         )}
         {status === 'success' && campaigns.length === 0 && (
