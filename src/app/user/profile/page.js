6325152ce0ed1d1019383f90aa46560d99699031
Amalia F <amaliasaleena19@gmail.com>
2024-04-16 22:14:15 +0530

    profile

--- src/app/user/profile/page.js
+++ src/app/user/profile/page.js
@@ -5,9 +5,6 @@ const Page = () => {
   return (
     <PaddingLayout>
       <div>
-        <h1 className="w-full text-center text-2xl font-bold lg:text-4xl">
-          My Profile
-        </h1>
         <Profile />
       </div>
     </PaddingLayout>
