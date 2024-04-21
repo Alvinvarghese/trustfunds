61da61301e491ce44292949a15e1be5e1224ed02
Alvin Varghese <alvin19official@gmail.com>
2024-04-21 21:48:31 +0530

    feat: fund campaign page

--- src/app/campaigns/[slug]/fund/page.js
+++ src/app/campaigns/[slug]/fund/page.js
@@ -1,14 +1,19 @@
 "use client";
 
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useCampaignDetailContext } from "@/context/CampaignContext";
-import { useState } from "react";
 
 const Page = () => {
   const { status, campaignData, fetchCampaignData } =
     useCampaignDetailContext();
   const [contributionAmount, setContributionAmount] = useState("");
+
+  const fundCampaign = async (amount) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log(`Funded ${amount} ETH to the campaign.`);
+  };
 
   const handleContribute = async () => {
     try {
@@ -20,76 +25,57 @@ const Page = () => {
     }
   };
 
-  const fundCampaign = async (amount) => {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    console.log(`Funded ${amount} ETH to the campaign.`);
-  };
-
-  // const sampleMilestones = [
-  //   { deadline: "2024-05-01", completionPercentage: 25 },
-  //   { deadline: "2024-05-15", completionPercentage: 50 },
-  //   { deadline: "2024-06-01", completionPercentage: 75 },
-  //   { deadline: "2024-06-15", completionPercentage: 100 },
-  // ];
-
   return (
     <>
-      <h1 className="pt-10 text-center text-3xl font-semibold">
+      <h1 className="pt-4 text-center text-xl font-semibold underline lg:pt-10 lg:text-3xl">
         Campaign Funds!
       </h1>
-      <div className="m-6 flex h-screen flex-col gap-6 p-10">
+      <div className="m-6 flex h-screen flex-col gap-4 lg:gap-6 lg:p-10">
         {status === "loading" && <p>Loading...</p>}
         {status === "error" && <p>Error fetching campaign data</p>}
         {status === "success" && campaignData && (
           <>
-            <h2>
-              {" "}
-              <span className="text-lg font-semibold">Recipient:</span>{" "}
-              {campaignData.recipient}
+            <h2 className="flex flex-col items-center justify-start gap-2 lg:flex-row">
+              <span className="text-sm font-semibold lg:text-lg">
+                Recipient:
+              </span>
+              <span>{campaignData.creator}</span>
             </h2>
-            <p>
-              {" "}
-              <span className="text-lg font-semibold">Target Amount:</span>{" "}
-              {campaignData.targetAmount}
+            <p className="flex flex-col items-center justify-start gap-2 lg:flex-row">
+              <span className="text-sm font-semibold lg:text-lg">
+                Target Amount:
+              </span>
+              <span>{campaignData.goal}</span>
             </p>
-            <p>
-              {" "}
-              <span className="text-lg font-semibold">Deadline:</span>{" "}
-              {campaignData.deadline}
+            <p className="flex flex-col items-center justify-start gap-2 lg:flex-row">
+              <span className="text-sm font-semibold lg:text-lg">
+                Deadline:
+              </span>
+              <span>{campaignData.endDate}</span>
             </p>
-            <p>
-              {" "}
-              <span className="text-lg font-semibold">Total Raised:</span>{" "}
-              {campaignData.totalRaised}
+            <p className="flex flex-col items-center justify-start gap-2 lg:flex-row">
+              <span className="text-sm font-semibold lg:text-lg">
+                Total Raised:
+              </span>
+              <span>{campaignData.totalRaised}</span>
             </p>
-            <p>
-              {" "}
-              <span className="text-lg font-semibold">Completed:</span>{" "}
-              {campaignData.completed ? "Yes" : "No"}
+            <p className="flex flex-col items-center justify-start gap-2 lg:flex-row">
+              <span className="text-sm font-semibold lg:text-lg">
+                Completion Status:
+              </span>
+              <span>{campaignData.completed ? "Complete" : "Incomplete"}</span>
             </p>
-            <h3>
-              {" "}
-              <span className="text-lg font-semibold">Milestones:</span>{" "}
-            </h3>
-            <ul>
-              {campaignData.milestones.map((milestone, index) => (
-                <li key={index}>
-                  Deadline: {milestone.deadline}, Completion Percentage:{" "}
-                  {milestone.completionPercentage}%
-                </li>
-              ))}
-            </ul>
 
-            <div className="flex flex-row gap-10">
+            <div className="flex flex-col gap-1 lg:flex-row lg:gap-5">
               <Input
                 type="number"
                 value={contributionAmount}
                 onChange={(e) => setContributionAmount(e.target.value)}
                 placeholder="Enter amount to contribute"
-                className="w-3/4"
+                className="lg:w-[300px]"
               />
-              <Button onClick={handleContribute} className="w-1/4">
-                Fund
+              <Button onClick={handleContribute} className="w-fit">
+                Fund this campaign
               </Button>
             </div>
           </>
